6285983b57488fd3c79934e3323d3be746fbd537
shangria <shangrialena@gmail,com>
2020-01-07 18:00:27 +0200

    HW2_1_2

---
+++ HW2_1/2_1_2.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 5 ++i) {  // выведет Uncaught SyntaxError: Invalid left-hand side expression in postfix operation
+    console.log(i);
+}
+
+var  a=8 var b=5
+console.log(a+b) // выведет Uncaught SyntaxError: Unexpected token 'var'
