bceed2d55bc9a4353870170fa9b6eb91451fe715
shangria <shangrialena@gmail,com>
2020-01-07 19:49:43 +0200

    HW2_1_7

--- HW2_1/2_1_7.js
+++ HW2_1/2_1_7.js
@@ -1,6 +1,8 @@
 var numberUser = prompt('Введите число');
-if (parseFloat(numberUser)) {
+if (numberUser === true) {
     alert('Спасибо, вы ввели все правильно!')
+} else if (parseFloat(numberUser)) {
+    alert('Так не нужно! Внимательно вводи числа!')
 } else {
     alert('Введите число')
 }
