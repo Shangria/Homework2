bf4cc4bcc0f40abb3187d2e844ca0346e823cb96
Shangria <46632810+Shangria@users.noreply.github.com>
2020-01-08 20:09:58 +0200

    HW2_1_15

--- HW2_1/2_1_15.js
+++ HW2_1/2_1_15.js
@@ -1,10 +1,10 @@
 var christmasMovies = ['Один дома', 'Эльф', 'Рождественские каникулы', 'Подарок на рождество'];
-var movies = [];
+var movies = '';
 for (var j = 0; j < 2; j++) {
     movies += christmasMovies[j];
 }
 console.log(movies);
 
-var blindChrMovies = christmasMovies[2]+movies;
+christmasMovies[2] += movies;
 
-alert(blindChrMovies);
+alert(christmasMovies);
