ad429d14ffa417e8bf84777fc07b8cf25abc34fd
shangria <shangrialena@gmail,com>
2020-01-09 00:17:07 +0200

    HW2_2_6

---
+++ HW2_2/2_2_6.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 2;
+var sum = a + b;
+var sub = a - b;
+var multiplic = a * b;
+var divis = a / b;
+
+alert('Сумма' + ': ' + sum + ' ' + 'Вычитание' + ': ' + sub + ' ' + 'Умножение' + ': ' + multiplic + ' ' + 'Деление' + ':' + divis);
