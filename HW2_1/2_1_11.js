980d92cb24d594a8636f55cfc854fa8d815a9af3
shangria <shangrialena@gmail,com>
2020-01-09 22:52:48 +0200

    HW2_1_11

--- HW2_1/2_1_11.js
+++ HW2_1/2_1_11.js
@@ -1,3 +1,4 @@
 var answer1 = confirm('Быть или казаться?');
 var answer2 = confirm('Ты человек?');
 console.log(answer1+'\n'+ answer2);
+//возвращает логическое значение
--- HW2_1/2_1_9.js
+++ HW2_1/2_1_9.js
@@ -1,5 +1,5 @@
 var enteredText = prompt('Введите Вашу любимую цитату, используя словосочетание моя цитата, неиспользуя слово трамвай, волна ');
-
+debugger;
 if (enteredText.includes('моя цитата')) {
     alert('Спасибо, все верно!')
 } else if (enteredText.includes('трамвай')) {
