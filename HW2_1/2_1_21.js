73ffa4089e76b9dcb48519749fae11e62a1240cd
shangria <shangrialena@gmail,com>
2020-01-10 01:30:17 +0200

    HW 2.2.21

---
+++ HW2_1/2_1_21.js
@@ -0,0 +1,27 @@
+// Подумайте о том, как можно применить объекты к предыдущем заданию.
+var size = {
+    rusS: 40,
+    rusM: 42,
+    rusL: 46,
+    rusXL: 50
+};
+debugger;
+var calcSize;
+var ourSize = prompt('Введите Ваш размер по американской системе. Тоесть: S,M,L,XL');
+
+if (ourSize.slice(0) == "S") {
+    calcSize = size.rusS - 34;
+    alert('Ваш размер США ' + calcSize)
+}
+if (ourSize.slice(0) == "M") {
+    calcSize = size.rusM - 34;
+    alert('Ваш размер США ' + calcSize)
+}
+if (ourSize.slice(0) == "L") {
+    calcSize = size.rusL - 34;
+    alert('Ваш размер США ' + calcSize)
+}
+if (ourSize.slice(0) == "XL") {
+    calcSize = size.rusXL - 34;
+    alert('Ваш размер США ' + calcSize)
+}
