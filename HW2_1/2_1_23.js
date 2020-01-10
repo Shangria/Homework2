3291b57f8883eaaf05b98918488291d6c6155640
Shangria <46632810+Shangria@users.noreply.github.com>
2020-01-10 17:36:48 +0200

    Update 2_1_23.js

--- HW2_1/2_1_23.js
+++ HW2_1/2_1_23.js
@@ -1,13 +1,25 @@
 // Сделайте калькулятор, который позволит вам исходя из информации о количества этажей в доме и количества квартир на этаже
 // // // находить подъезд и этаж определенной квартиры по её номеру. Например для 9этажного дома по 4 квартиры на этаж 81 квартира находится на 3м этаже третьего подъезда.
 
-
-var storeys = prompt('кол-во этажей');
-var apartment = prompt(' номер кв.');
-var apartmentsStorey = prompt('кол-во квартир на этаже');
-var appatmrntsInPorch = storeys * apartmentsStorey; //кол-во квартир в подьезде
-var porch = Math.floor(((apartment - 1) / appatmrntsInPorch) + 1); //номер подьезда
-var porchRem = String(((apartment - 1) / appatmrntsInPorch) + 1).split(".")[1].substr(0, 1);//остаток от деление для вычисл
-var numberStore = Math.round(porchRem / apartmentsStorey + 1);// номер этажа
-
-alert(porch + ' ' + 'подьезд' + ' ' + numberStore + ' ' + 'этаж');
+var floorsCount = prompt('введите количество этажей в дому');
+var flatsPerFloorCount = prompt('введите количество квартир на этаже');
+var floorNumber = prompt('введите номер квартиры');
+// количество квартир в подъезде
+var floorsPerPorch = floorsCount * flatsPerFloorCount;
+var porch;
+var floor;
+porch = floorNumber / floorsPerPorch;
+if (String(porch).indexOf('.') !== -1) {
+    porch = Math.ceil(porch);
+}
+// номер квартиры в конкретном по в подъезде
+// т.е. к примеру количество этажей - 9 и количество квартир на этаже - 4, т.е. количевство квартир в подъезде - 36
+// кв 1 будет на 1м этаже в 1м подьезде - это 1я квартира в подъезде (и 1я в доме)
+// кв 37 будет на 1м этаже во 2м подьезде - это 1я квартира в подъезде (и 37я в доме)
+// кв 39 будет на 1м этаже во 2м подьезде - это 3я квартира в подъезде (и 39я в доме)
+var floorNumberInCurrentPorch = floorNumber - (porch - 1) * floorsPerPorch;
+floor = floorNumberInCurrentPorch / flatsPerFloorCount;
+if (String(floor).indexOf('.') !== -1) {
+    floor = Math.ceil(floor);
+}
+alert('подъезд ' + porch + ', этаж: ' + floor);
